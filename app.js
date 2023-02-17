b92885bc82c2f91238d11d6afd5602002e9bb6ed
rafaelbittencourt3 <117769410+rafaelbittencourt3@users.noreply.github.com>
2023-02-16 23:41:41 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+Vue.component('example-component', ExampleComponent);
+
+const app = new Vue({
+  el: '#app',
+});
