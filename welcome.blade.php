6a7f9a9c666695933dcbe8a72ddff2aa767e7ab6
rafaelbittencourt3 <117769410+rafaelbittencourt3@users.noreply.github.com>
2023-02-16 23:55:07 -0300

    Create welcome.blade.php

---
+++ welcome.blade.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>My App</title>
+  </head>
+  <body>
+    <div id="app">
+      <example-component></example-component>
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
+  </body>
+</html>
